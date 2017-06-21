3979f255a81f44242ffd4d296950320a17adfe3d
Yu-An Chen <atwood12@gmail.com>
2017-06-20 21:50:41 -0500

    update app  module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WikiComponent } from './wiki/wiki.component';
+import { WikiSmartComponent } from './wiki-smart/wiki-smart.component';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		WikiComponent,
+		WikiSmartComponent
 	],
 	imports: [
 		BrowserModule,
 		BrowserAnimationsModule,
 		FormsModule,
-		HttpModule
+		HttpModule,
+		JsonpModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
